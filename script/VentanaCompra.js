eeb2013443a89913f3de9318b2d5cd3f7d1c2d9e
luis alfredo pacheco sandoval <laps1308@gmail.com>
2020-04-24 08:14:27 -0500

    creacion de modulo de compra y añadido a carrito

--- script/VentanaCompra.js
+++ script/VentanaCompra.js
@@ -1,17 +1,37 @@
-function payBuy(){
-    var body=document.querySelector("body");
-    var item=document.querySelector("item");
-    var panel= document.createElement("div");
-    var ventana= document.createElement("section");
-    body.appendChild(panel);
-    panel.appendChild(ventana);
-    seccion=document.querySelector("div section");
-    div=document.querySelector("body div");
-    div.setAttribute("id","pantalla");
-    seccion.setAttribute("id","ventana");
-    pantalla=document.querySelector("#pantalla");
-    pantalla.style.width=""+screen.width+"px";
-    pantalla.style.height=""+screen.height+"px"
-    pantalla.style.backgroundColor="rgba(0,0,0,0.1)";
+function payBuy(imagen1="camarax.jpg",nombre="camara X",precio=35000,descripcion="Es una camara"){
+
+    var seccion=document.createElement("seccion");
+    var imagen=document.createElement("img");
+    var contenido=document.createElement("div");
+    var datos=document.createElement("P");
+    var cerrar=document.createElement("i");
+    var agregarCarrito=document.createElement("i");
+    var comprar=document.createElement("i");
+    //definicion de id y clases
+    cerrar.setAttribute("class","fas fa-times");
+    agregarCarrito.setAttribute("class","fas fa-shopping-cart");
+    comprar.setAttribute("class","fas fa-money-bill");
+    datos.setAttribute("id","datos");
+    contenido.setAttribute("id","contenido");
+    seccion.setAttribute("id","ventanaCompra");
+    imagen.setAttribute("id","imagen");
+    imagen.setAttribute("src","../resource/"+imagen1);
+    // finalizacion de definicion de atributps
+    datos.innerHTML="<strong>Nombre: </strong>"+nombre+"<br><strong>Precio: </strong>"+precio+"<br><Strong>Descripcion: </strong>"+descripcion+"";
+    document.body.appendChild(seccion);
+    seccion.appendChild(contenido);
+    seccion.appendChild(imagen);
+    seccion.appendChild(cerrar);
+    seccion.appendChild(agregarCarrito);
+    seccion.appendChild(comprar);
+    contenido.appendChild(datos);
+
+// eventos en botones
+  //cerrar.addEventListener("click",closeWindow()); 
 } 
+function closeWindow(){ 
+    var pantalla=document.getElementById("pantalla");
+    pantalla.remove();
+    
+}
 
